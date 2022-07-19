f458e49d308c5573be483d6642c7f566b88c81d2
heyylateef <lateef.lotz@gmail.com>
2022-07-19 18:35:46 -0400

    updated index.html and app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [isLoading, setLoading] = useState(true);
+  
+  function someRequest() { //Simulates a request; makes a "promise" that'll run for 2.5 seconds
+    return new Promise(resolve => setTimeout(() => resolve(), 2500));
+  } 
+
+  useEffect(() => {
+    someRequest().then(() => {
+      const loaderElement = document.querySelector(".loader-container");
+      if (loaderElement) {
+        loaderElement.remove();
+        setLoading(!isLoading);
+      }
+    });
+  });
+
+  if (isLoading) { //
+    return null;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I love <a href="https://lateeflab.com"> Lateef Lab!</a>
         </p>
         <a
           className="App-link"
